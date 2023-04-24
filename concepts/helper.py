ea309b1c0adc7cf71256b1cb85aa479bb34c45e7
Jon <jhanek@cecco.com>
2023-04-24 15:17:34 -0500

    initial commit

---
+++ concepts/helper.py
@@ -0,0 +1,6 @@
+def test123():
+    print("test123 succesfully imported")
+
+if __name__ == "__main__":
+    # This line only runs if we run helper.py DIRECTLY
+    print('Print statement from helper.py')
